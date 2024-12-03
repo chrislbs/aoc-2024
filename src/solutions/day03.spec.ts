f7dcd95bdac95b6f659df7c240bfede0b24e3e0b
Chris Pounds <chris.pounds@experoinc.com>
2024-12-03 08:47:41 -0600

    Day3 Part2 Complete

--- src/solutions/day03.spec.ts
+++ src/solutions/day03.spec.ts
@@ -1,14 +1,18 @@
-import {parseInput} from "./day03";
+import {parseInputP1, p2pipeline} from "./day03";
 
-const exampleInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const p1ExampleInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
 
 test('day3 part1 example works', () => {
-    const result = parseInput(exampleInput).map(pair => pair[0] * pair[1]).reduce(
+    const result = parseInputP1(p1ExampleInput).reduce(
         (acc, val) => acc + val,
         0);
 
     expect(result).toEqual(161);
 })
 
+const p2ExampleInput = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+
 test('day3 part2 example works', () => {
+    const result = p2pipeline(p2ExampleInput);
+    expect(result).toEqual(48)
 })
--- src/solutions/day03.ts
+++ src/solutions/day03.ts
@@ -2,32 +2,69 @@ import {Solution} from "./solution";
 import {readPuzzleInput} from "../utils/files";
 import assert from "node:assert";
 
-export function parseInput(input: string): number[][] {
-    const matches = input.match(/mul\(\d+,\d+\)/g);
+const mulRegex = /mul\(\d+,\d+\)/g;
+const doRegex = /do\(\)/g;
+const dontRegex = /don't\(\)/g;
+
+export const allInstructions = [mulRegex, doRegex, dontRegex];
+
+export function parseInstructions(input: string, instructionRegexes: RegExp[]): string[] {
+    const combinedRe = new RegExp(instructionRegexes.map(re => re.source).join("|"), "g");
+    const matches = input.match(combinedRe);
     assert(matches !== null);
 
-    const numPairs = new Array<number[]>(matches.length);
+    return matches.map(x => x);
+}
+
+function runMultiInstruction(instruction: string): number {
+    const [lhs, rhs] = instruction.replace("mul(", "").replace(")", "").split(",").map(x => parseInt(x, 10));
+    return lhs * rhs;
+}
+
+export function parseInputP1(input: string): number[] {
+    const matches = input.match(mulRegex);
+    assert(matches !== null);
+
+    const results = new Array<number>(matches.length);
     for(let i = 0; i < matches.length; i++) {
         const match = matches[i];
-        const numPair = match.replace("mul(", "")
-            .replace(")", "")
-            .split(",")
-            .map(x => parseInt(x, 10));
+        const result = runMultiInstruction(match);
 
-        numPairs[i] = numPair;
+        results[i] = result;
     }
 
-    return numPairs;
+    return results;
 }
 
 function part1(): number {
-    return parseInput(readPuzzleInput(3))
-        .map(pair => pair[0] * pair[1])
+    return parseInputP1(readPuzzleInput(3))
         .reduce((total, val) => total + val, 0);
 }
 
+export function p2pipeline(input: string): number {
+    const instructions = parseInstructions(input, allInstructions);
+
+    let mulEnabled = true;
+    let total = 0;
+    for(let i = 0; i < instructions.length; i++) {
+        const instruction = instructions[i];
+
+        if(instruction.match(mulRegex)) {
+            if(mulEnabled) {
+                total += runMultiInstruction(instruction);
+            }
+        } else if (instruction.match(doRegex)) {
+            mulEnabled = true;
+        } else if (instruction.match(dontRegex)) {
+            mulEnabled = false;
+        }
+    }
+
+    return total;
+}
+
 function part2(): number {
-    return 1;
+    return p2pipeline(readPuzzleInput(3));
 }
 
 const day3: Solution = {
